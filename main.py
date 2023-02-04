2acc46d06a0bffacb43502cd2d922717a3f6961f
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2023-02-03 19:09:10 -0600

    Documentation, documentation..

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import base64
 import requests
 
+# Creating token and encoding with Base64 encryption
 token = f"Bearer {base64.b64encode(b'Software Test Engineer').decode('utf-8')}"
+
+# Setting API URL variable
 url = 'https://flightaware.com/about/careers/position/_someURLforAPI_'
+
+# Setting API transaction attributes
 headers = {
     "content-type": "application/json",
     "Authorization": token
@@ -12,4 +17,5 @@ json = {
     "email": "Your@email.com"
 }
 
+# Posting API call
 print(requests.post(url, headers=headers, json=json).text)
